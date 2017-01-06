b9f5ae7260edfd3b18f58bfdfd0779f69e44aa03
Josué Chávez <geovannijosue.chavez@outlook.com>
2017-01-06 17:25:24 -0600

    Second commit

--- app/src/main/java/com/globalpaysolutions/yocomprorecarga/ui/activities/AcceptTerms.java
+++ app/src/main/java/com/globalpaysolutions/yocomprorecarga/ui/activities/AcceptTerms.java
@@ -1,17 +1,29 @@
 package com.globalpaysolutions.yocomprorecarga.ui.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.globalpaysolutions.yocomprorecarga.R;
 
 public class AcceptTerms extends AppCompatActivity
 {
+    Button btnAccept;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accept_terms);
+
+        btnAccept = (Button) findViewById(R.id.btnAcceptTerms);
+    }
+
+    public void acceptTerms(View view)
+    {
+        Intent accept = new Intent(AcceptTerms.this, Home.class);
+        startActivity(accept);
     }
 }
