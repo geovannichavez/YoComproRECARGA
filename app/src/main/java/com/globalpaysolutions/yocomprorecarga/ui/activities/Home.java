6ed8693a38a55bbc89b7345aa4ed906003161726
Josué Chávez <geovannijosue.chavez@outlook.com>
2017-01-12 17:30:54 -0600

    Se añadió layout de RequestTopup, se añadieron botones en mapa. Se añadieron drawables. Otras mejoras

--- app/src/main/java/com/globalpaysolutions/yocomprorecarga/ui/activities/Home.java
+++ app/src/main/java/com/globalpaysolutions/yocomprorecarga/ui/activities/Home.java
@@ -1,13 +1,18 @@
 package com.globalpaysolutions.yocomprorecarga.ui.activities;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.ShareCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.globalpaysolutions.yocomprorecarga.R;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -22,6 +27,8 @@ public class Home extends AppCompatActivity implements OnMapReadyCallback
     //Adapters y Layouts
     private Toolbar toolbar;
     private GoogleMap googleMap;
+    private Button btnRequestTopup;
+    private ImageButton btnVirtualReality;
 
     private static final LatLng SYDNEY = new LatLng(-33.88,151.21);
     private static final LatLng AV_LA_CAPILLA = new LatLng(13.686005,-89.242484);
@@ -34,6 +41,9 @@ public class Home extends AppCompatActivity implements OnMapReadyCallback
         toolbar = (Toolbar) findViewById(R.id.homeToolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle(null);
+
+        btnRequestTopup = (Button) findViewById(R.id.btnRequestTopoup);
+        btnVirtualReality = (ImageButton) findViewById(R.id.btnVirtualReality);
 
         InitilizeMap();
 
@@ -83,6 +93,37 @@ public class Home extends AppCompatActivity implements OnMapReadyCallback
                 //.tilt(30)                   // Sets the tilt of the camera to 30 degrees
                 .build();                   // Creates a CameraPosition from the builder
         googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
+    }
+
+    public void RequestTopup(View view)
+    {
+        Intent requestTopup = new Intent(Home.this, RequestTopup.class);
+        startActivity(requestTopup);
+    }
+
+    public void OpenCamera(View view)
+    {
+        if (Build.VERSION.SDK_INT >= 23 &&
+                ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
+        {
+            if (!shouldShowRequestPermissionRationale(Manifest.permission.CAMERA))
+            {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 3);
+            }
+        }
+        else
+        {
+            try
+            {
+                Intent camera = new Intent("android.media.action.IMAGE_CAPTURE");
+                startActivity(camera);
+            }
+            catch (Exception ex)
+            {
+                ex.printStackTrace();
+            }
+        }
 
     }
 }
