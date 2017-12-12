3c4fe9c06f4c4fce63f57a58d39c76045b71c922
Josué Chávez <geovannijosue.chavez@outlook.com>
2017-12-12 15:03:01 -0600

    # Se cambió la Support Library a versión 26.0.2. # Se implementó notificaciones con imágenes

--- app/src/main/java/com/globalpaysolutions/yocomprorecarga/utils/OneSignalNotificationExtender.java
+++ app/src/main/java/com/globalpaysolutions/yocomprorecarga/utils/OneSignalNotificationExtender.java
@@ -1,13 +1,20 @@
 package com.globalpaysolutions.yocomprorecarga.utils;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.content.ContextCompat;
+import android.text.TextUtils;
 import android.util.Log;
 
 import com.globalpaysolutions.yocomprorecarga.R;
 import com.onesignal.NotificationExtenderService;
 import com.onesignal.OSNotificationDisplayedResult;
 import com.onesignal.OSNotificationReceivedResult;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 /**
  * Created by Josué Chávez on 22/03/2017.
@@ -19,9 +26,13 @@ public class OneSignalNotificationExtender extends NotificationExtenderService
 
     final static String GROUP_KEY_NOTIF = "yocomprorecarga_default_notification";
 
+    private OSNotificationReceivedResult mNotification;
+
     @Override
     protected boolean onNotificationProcessing(final OSNotificationReceivedResult notification)
     {
+        mNotification = notification;
+
         OverrideSettings overrideSettings = new OverrideSettings();
 
         overrideSettings.extender = new NotificationCompat.Extender()
@@ -29,11 +40,34 @@ public class OneSignalNotificationExtender extends NotificationExtenderService
             @Override
             public NotificationCompat.Builder extend(NotificationCompat.Builder builder)
             {
-                return builder.setColor(ContextCompat.getColor(getApplicationContext(), R.color.AppGreen))
+                builder.setColor(ContextCompat.getColor(getApplicationContext(), R.color.AppGreen))
                         .setAutoCancel(true)
                         .setGroupSummary(true)
                         .setGroup(GROUP_KEY_NOTIF)
                         .setContentText(notification.payload.body);
+
+                if(!TextUtils.isEmpty(mNotification.payload.bigPicture))
+                {
+                    Bitmap image = getBitmapfromUrl(mNotification.payload.bigPicture);
+
+                    if(image != null)
+                    {
+                        final NotificationCompat.BigPictureStyle bigPictureStyle = new NotificationCompat.BigPictureStyle();
+                        bigPictureStyle.bigPicture(image);
+                        bigPictureStyle.setSummaryText(mNotification.payload.body);
+                        builder.setStyle(bigPictureStyle);
+                    }
+                    else
+                    {
+                        builder.setStyle(new NotificationCompat.BigTextStyle().bigText(mNotification.payload.body));
+                    }
+                }
+                else
+                {
+                    builder.setStyle(new NotificationCompat.BigTextStyle().bigText(mNotification.payload.body));
+                }
+
+                return builder;
             }
         };
 
@@ -41,5 +75,27 @@ public class OneSignalNotificationExtender extends NotificationExtenderService
         Log.d(TAG, "Notification displayed with id: " + displayedResult.androidNotificationId);
 
         return true;
+    }
+
+    /*
+    * To get a Bitmap image from the URL received
+    * */
+    public Bitmap getBitmapfromUrl(String imageUrl)
+    {
+        try
+        {
+            URL url = new URL(imageUrl);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoInput(true);
+            connection.connect();
+            InputStream input = connection.getInputStream();
+            return BitmapFactory.decodeStream(input);
+
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
